aef061fce6ecd00afb6eac06479e07b8e7e3e54f
maxmhoff <maxmhoff@sol.dk>
2023-08-03 19:11:00 +0200

    Add camera movement on 'finished'

--- src/lib/components/3d-experience/Camera.svelte
+++ src/lib/components/3d-experience/Camera.svelte
@@ -1,13 +1,33 @@
 <script>
+	import { gameState } from '$lib/stores/gameStore';
 	import { T } from '@threlte/core';
+	import { linear, quadIn, quadInOut, quadOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+	let camera;
+	const animDurationFast = 1000;
+	const animDurationSlow = 7000;
+	const defaultPosition = { x: 0, y: 87, z: 35 };
+	const defaultRotationX = -1.1726917666035248;
+	const cameraPositionY = tweened(defaultPosition.y, { duration: animDurationFast, easing: quadInOut });
+	const cameraPositionZ = tweened(defaultPosition.z, { duration: animDurationFast, easing: quadInOut });
+	const cameraRotationX = tweened(defaultRotationX, { duration: animDurationFast, easing: quadInOut });
+	$: {
+		if ($gameState === 'finished') {
+			cameraPositionY.set(20, {duration: animDurationSlow});
+			cameraPositionZ.set(40, {duration: animDurationSlow});
+			cameraRotationX.set(-.5, {duration: animDurationSlow});
+		} else {
+			cameraPositionY.set(defaultPosition.y);
+			cameraPositionZ.set(defaultPosition.z);
+			cameraRotationX.set(defaultRotationX);
+		}
+	}
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
 	fov={50}
-	position.y={87}
-	position.z={35}
-	on:create={({ ref }) => {
-		ref.lookAt(0, -20, -10);
-	}}
+	position.y={$cameraPositionY}
+	position.z={$cameraPositionZ}
+	rotation.x={$cameraRotationX}
 />
