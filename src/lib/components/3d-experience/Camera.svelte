243a8dbe60cc2bd51a3aa043b4e4892aa872801d
maxmhoff <maxmhoff@sol.dk>
2023-08-03 21:58:16 +0200

    Movement on tile active state

--- src/lib/components/3d-experience/Camera.svelte
+++ src/lib/components/3d-experience/Camera.svelte
@@ -1,13 +1,14 @@
 <script>
-	import { gameState } from '$lib/stores/gameStore';
+	import { gameState, tiles } from '$lib/stores/gameStore';
 	import { T } from '@threlte/core';
 	import { linear, quadIn, quadInOut, quadOut } from 'svelte/easing';
 	import { tweened } from 'svelte/motion';
 	let camera;
 	const animDurationFast = 1000;
 	const animDurationSlow = 7000;
-	const defaultPosition = { x: 0, y: 87, z: 35 };
+	const defaultPosition = { x: 0, y: 72, z: 30 };
 	const defaultRotationX = -1.1726917666035248;
+	const cameraPositionX = tweened(0, { duration: animDurationFast, easing: quadInOut });
 	const cameraPositionY = tweened(defaultPosition.y, { duration: animDurationFast, easing: quadInOut });
 	const cameraPositionZ = tweened(defaultPosition.z, { duration: animDurationFast, easing: quadInOut });
 	const cameraRotationX = tweened(defaultRotationX, { duration: animDurationFast, easing: quadInOut });
@@ -17,16 +18,28 @@
 			cameraPositionZ.set(40, {duration: animDurationSlow});
 			cameraRotationX.set(-.5, {duration: animDurationSlow});
 		} else {
+			cameraPositionX.set(0);
 			cameraPositionY.set(defaultPosition.y);
 			cameraPositionZ.set(defaultPosition.z);
 			cameraRotationX.set(defaultRotationX);
 		}
+	}
+
+	$: {
+		$tiles.forEach(tile => {
+			if(tile.state === 'active') {
+				cameraPositionX.set(tile.position.x / 3, {duration: 600, easing: quadOut});
+				cameraPositionZ.set(defaultPosition.z + tile.position.z / 3, {duration: 600, easing: quadOut});
+				cameraPositionY.set(defaultPosition.y + tile.position.z / 10, {duration: 600, easing: quadOut});
+			}
+		})
 	}
 </script>
 
 <T.PerspectiveCamera
 	makeDefault
 	fov={50}
+	position.x={$cameraPositionX}
 	position.y={$cameraPositionY}
 	position.z={$cameraPositionZ}
 	rotation.x={$cameraRotationX}
