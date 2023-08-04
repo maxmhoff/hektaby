d90ef98c325a0ac31efac6bce953f3653fbccf61
maxmhoff <maxmhoff@sol.dk>
2023-08-04 08:04:43 +0200

    Make island bigger

--- src/lib/components/3d-experience/Island.svelte
+++ src/lib/components/3d-experience/Island.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <T.Mesh rotation.x={DEG2RAD * -90} position.y={-5}>
-    <T.CircleGeometry args={[33, 64, 0, 360 * DEG2RAD]}  />
+    <T.CircleGeometry args={[40, 64, 0, 360 * DEG2RAD]}  />
     <T.MeshStandardMaterial color="#30914c" />
 </T.Mesh>
 <T.Mesh rotation.x={DEG2RAD * -90} position.y={-5}>
-    <T.RingGeometry args={[33, 63, 64, 360 * DEG2RAD]} />
+    <T.RingGeometry args={[40, 63, 64, 360 * DEG2RAD]} />
     <T.MeshStandardMaterial color="#4a9db0" />
 </T.Mesh>
