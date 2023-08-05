1af62ba9ff2f8a2da38a4fe9ff0fc9cd671e6deb
maxmhoff <maxmhoff@sol.dk>
2023-08-05 12:22:34 +0200

    Redirect from frontpage

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import createSeed from '$lib/utilities/create-seed';
+
+export const load = (async () => {
+	throw redirect(308, `${createSeed()}`)
+}) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { goto } from "$app/navigation";
-	import createSeed from "$lib/utilities/create-seed";
-</script>
-
-<p>Frontpage</p>
-
-<button on:click={() => goto(`/${createSeed()}`)}>Start</button>
-
