d5bc2d93a8405b8eb64cb5936e37c22d8d3871a6
maxmhoff <maxmhoff@sol.dk>
2023-08-06 16:15:08 +0200

    Update zonecolors

--- src/lib/data/zone-colors.ts
+++ src/lib/data/zone-colors.ts
@@ -1,18 +1,21 @@
 const zoneColors = {
     default: {
-        base: '#DDDDDD',
+        base: '#CCCCCC',
     },
     residential: {
         base: '#4fbd4f',
         selected: '#aaf0aa',
+        hud: '#7cca6b',
     },
     commercial: {
         base: '#5c9bbf',
         selected: '#a9d1e8', 
+        hud: '#77b1c9',
     },
     industrial: {
         base: '#9c7c56',
         selected: '#d4b998',
+        hud: '#b19165',
     },
     special: {
         base: '#666666',
