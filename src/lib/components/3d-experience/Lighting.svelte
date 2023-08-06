1c1bd732503a48bb9ac24d980b728f0147de39b7
maxmhoff <maxmhoff@sol.dk>
2023-08-06 16:14:53 +0200

    Lower the ligthing 🌜

--- src/lib/components/3d-experience/Lighting.svelte
+++ src/lib/components/3d-experience/Lighting.svelte
@@ -2,10 +2,10 @@
 	import { T } from '@threlte/core';
 </script>
 
-<T.AmbientLight intensity={0.8} />
+<T.AmbientLight intensity={0.5} />
 <T.DirectionalLight
 	position={[0, 14, 10]}
-	intensity={.8}
+	intensity={.7}
 	castShadow
 	shadow.camera.top={22}
 	shadow.camera.right={22}
