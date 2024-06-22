800f7bd354fae77556f2aaab5a09c179d9557ed5
maxmhoff <maxmhoff@sol.dk>
2024-06-22 12:15:01 +0200

    build: update packages

--- src/lib/components/3d-experience/Lighting.svelte
+++ src/lib/components/3d-experience/Lighting.svelte
@@ -2,10 +2,10 @@
 	import { T } from '@threlte/core';
 </script>
 
-<T.AmbientLight intensity={0.5} />
+<T.AmbientLight intensity={1.5} />
 <T.DirectionalLight
 	position={[0, 14, 10]}
-	intensity={.7}
+	intensity={2.2}
 	castShadow
 	shadow.camera.top={22}
 	shadow.camera.right={22}
