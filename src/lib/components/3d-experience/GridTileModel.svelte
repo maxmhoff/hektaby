64a3d824982f6f77b5c6075ce2a61980420920da
maxmhoff <maxmhoff@sol.dk>
2024-06-23 16:32:12 +0200

    fix: a few changes to the park mesh

--- src/lib/components/3d-experience/GridTileModel.svelte
+++ src/lib/components/3d-experience/GridTileModel.svelte
@@ -204,157 +204,157 @@
 {/if}
 
 {#if tileType === 'park' && $park}
-<T.Group position.x={.3} position.z={.2} rotation.y={DEG2RAD * 90}>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cube001.geometry}
-	  material={$park.materials.dark_green}
-	  position={[-0.321, 0.998, 0.824]}
-	  rotation={[0, -0.35, -0.227]}
-	  scale={[0.019, 0.09, 1.541]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cube002.geometry}
-	  material={$park.materials.dark_green}
-	  position={[-0.509, 0.447, 0.778]}
-	  rotation={[0, -0.35, -1.707]}
-	  scale={[0.02, 0.087, 1.541]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cube003.geometry}
-	  material={$park.materials.dark_green}
-	  position={[0.086, 0.752, -0.448]}
-	  rotation={[0, -0.35, -0.233]}
-	  scale={[0.01, 0.339, 0.077]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cube004.geometry}
-	  material={$park.materials.dark_green}
-	  position={[-0.174, 0.47, -0.548]}
-	  rotation={[0, -0.35, 1.422]}
-	  scale={[0.01, 0.231, 0.077]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cylinder.geometry}
-	  material={$park.materials.steel}
-	  position={[-0.025, 0.218, -0.495]}
-	  rotation={[0, -0.35, 0]}
-	  scale={[0.866, 1, 0.693]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cylinder001.geometry}
-	  material={$park.materials.steel}
-	  position={[-0.945, 0.217, 2.035]}
-	  rotation={[0, -0.35, 0]}
-	  scale={[0.866, 1, 0.693]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cylinder002.geometry}
-	  material={$park.materials.steel}
-	  position={[-0.458, 0.26, -0.645]}
-	  rotation={[0, -0.35, 0]}
-	  scale={[0.77, 1.19, 0.616]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cylinder003.geometry}
-	  material={$park.materials.steel}
-	  position={[-1.387, 0.255, 1.883]}
-	  rotation={[0, -0.35, 0]}
-	  scale={[0.77, 1.184, 0.616]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Cube.geometry}
-	  material={$park.materials.wood}
-	  position={[0.751, -0.024, -2.411]}
-	  rotation={[Math.PI, -0.248, Math.PI]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere.geometry}
-	  material={$park.materials.green1}
-	  position={[2.598, 5.64, -1.943]}
-	  rotation={[2.975, -0.233, 3.029]}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere001.geometry}
-	  material={$park.materials.green2}
-	  position={[-0.968, 5.635, -1.336]}
-	  rotation={[2.218, -0.799, 1.652]}
-	  scale={1.598}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere002.geometry}
-	  material={$park.materials.green2}
-	  position={[0.531, 5.623, -3.742]}
-	  rotation={[1.61, -1.028, 1.239]}
-	  scale={1.257}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere003.geometry}
-	  material={$park.materials.green1}
-	  position={[-0.303, 5.609, -3.005]}
-	  rotation={[2.064, -0.886, 1.56]}
-	  scale={1.388}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere004.geometry}
-	  material={$park.materials.green2}
-	  position={[0.772, 6.278, -1.754]}
-	  rotation={[1.975, -0.461, 1.035]}
-	  scale={1.708}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere005.geometry}
-	  material={$park.materials.green2}
-	  position={[1.596, 5.592, -2.998]}
-	  rotation={[0.927, -0.83, -1.325]}
-	  scale={1.371}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere007.geometry}
-	  material={$park.materials.green1}
-	  position={[0.14, 6.143, -0.301]}
-	  rotation={[2.119, -1.436, -0.002]}
-	  scale={1.396}
-	/>
-	<T.Mesh
-	  castShadow
-	  receiveShadow
-	  geometry={$park.nodes.Icosphere006.geometry}
-	  material={$park.materials.green1}
-	  position={[1.851, 5.914, -0.742]}
-	  rotation={[-3.019, -1.412, 1.456]}
-	  scale={1.409}
-	/>
+	<T.Group position.z={.2} rotation.y={DEG2RAD * 100}>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cube001.geometry}
+		  material={$park.materials.dark_green}
+		  position={[-0.321, 0.998, 0.824]}
+		  rotation={[0, -0.35, -0.227]}
+		  scale={[0.019, 0.09, 1.541]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cube002.geometry}
+		  material={$park.materials.dark_green}
+		  position={[-0.509, 0.447, 0.778]}
+		  rotation={[0, -0.35, -1.707]}
+		  scale={[0.02, 0.087, 1.541]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cube003.geometry}
+		  material={$park.materials.dark_green}
+		  position={[0.086, 0.752, -0.448]}
+		  rotation={[0, -0.35, -0.233]}
+		  scale={[0.01, 0.339, 0.077]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cube004.geometry}
+		  material={$park.materials.dark_green}
+		  position={[-0.174, 0.47, -0.548]}
+		  rotation={[0, -0.35, 1.422]}
+		  scale={[0.01, 0.231, 0.077]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cylinder.geometry}
+		  material={$park.materials.steel}
+		  position={[-0.025, 0.218, -0.495]}
+		  rotation={[0, -0.35, 0]}
+		  scale={[0.866, 1, 0.693]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cylinder001.geometry}
+		  material={$park.materials.steel}
+		  position={[-0.945, 0.217, 2.035]}
+		  rotation={[0, -0.35, 0]}
+		  scale={[0.866, 1, 0.693]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cylinder002.geometry}
+		  material={$park.materials.steel}
+		  position={[-0.458, 0.26, -0.645]}
+		  rotation={[0, -0.35, 0]}
+		  scale={[0.77, 1.19, 0.616]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cylinder003.geometry}
+		  material={$park.materials.steel}
+		  position={[-1.387, 0.255, 1.883]}
+		  rotation={[0, -0.35, 0]}
+		  scale={[0.77, 1.184, 0.616]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Cube.geometry}
+		  material={$park.materials.wood}
+		  position={[0.87, -0.004, -1.419]}
+		  rotation={[Math.PI, -0.248, Math.PI]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere.geometry}
+		  material={$park.materials.green1}
+		  position={[2.717, 5.661, -0.95]}
+		  rotation={[2.975, -0.233, 3.029]}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere001.geometry}
+		  material={$park.materials.green2}
+		  position={[-0.849, 5.655, -0.343]}
+		  rotation={[2.218, -0.799, 1.652]}
+		  scale={1.598}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere002.geometry}
+		  material={$park.materials.green2}
+		  position={[0.65, 5.644, -2.749]}
+		  rotation={[1.61, -1.028, 1.239]}
+		  scale={1.257}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere003.geometry}
+		  material={$park.materials.green1}
+		  position={[-0.184, 5.63, -2.012]}
+		  rotation={[2.064, -0.886, 1.56]}
+		  scale={1.388}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere004.geometry}
+		  material={$park.materials.green2}
+		  position={[0.891, 6.298, -0.762]}
+		  rotation={[1.975, -0.461, 1.035]}
+		  scale={1.708}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere005.geometry}
+		  material={$park.materials.green2}
+		  position={[1.715, 5.613, -2.005]}
+		  rotation={[0.927, -0.83, -1.325]}
+		  scale={1.371}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere007.geometry}
+		  material={$park.materials.green1}
+		  position={[0.259, 6.164, 0.692]}
+		  rotation={[2.119, -1.436, -0.002]}
+		  scale={1.396}
+		/>
+		<T.Mesh
+		  castShadow
+		  receiveShadow
+		  geometry={$park.nodes.Icosphere006.geometry}
+		  material={$park.materials.green1}
+		  position={[1.97, 5.935, 0.251]}
+		  rotation={[-3.019, -1.412, 1.456]}
+		  scale={1.409}
+		/>
   </T.Group>
 {/if}
