f46349840e1982bc83a7d542658037ee8ae7808c
maxmhoff <maxmhoff@sol.dk>
2024-06-24 14:57:05 +0200

    fix: increase height of light to avoid shadow rendering weird

--- src/lib/components/3d-experience/Lighting.svelte
+++ src/lib/components/3d-experience/Lighting.svelte
@@ -4,11 +4,14 @@
 
 <T.AmbientLight intensity={1.5} />
 <T.DirectionalLight
-	position={[0, 14, 10]}
+	position={[-2, 15, 10]}
 	intensity={2.2}
 	castShadow
 	shadow.camera.top={22}
 	shadow.camera.right={22}
 	shadow.camera.bottom={-22}
 	shadow.camera.left={-22}
+	shadow.mapSize.width={2048}
+	shadow.mapSize.height={2048}
+	shadow.bias={-0.001}
 />
